6f860079cda285f098aa2e93d835821a75b5e295
lirc572 <admin@lirc572.com>
2025-12-30 00:22:56 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'tomato',
+      short_name: 'tomato',
+      description: 'Your favourite Pomodoro timer.',
+      theme_color: '#FF6347',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
